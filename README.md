30c3b543ab60b9ec111e732305198c150659ed78
Rosalia <99089959+RosaliaLey@users.noreply.github.com>
2022-10-08 16:52:25 -0400

    initial commit for hidden search widget

---
+++ README.md
@@ -0,0 +1,24 @@
+# Hidden Search Widget
+
+Hidden search with an icon. The input field expands displaying a placeholder of "Search..." when clicking on the search icon.
+
+## Table of contents
+
+- [Screenshot](#screenshot)
+- [Links](#links)
+- [Built with](#built-with)
+
+### Screenshot
+
+![](img/hidden-search-widget.png)
+
+### Links
+
+- [Live Site URL](https://rosalialey.github.io/hidden-search-widget/)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JavaScript
---
+++ script.js
@@ -0,0 +1,35 @@
+const search = document.querySelector(".search");
+const input = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
+
+(function (document, window) {
+  if (!document || !window) {
+    return;
+  }
+
+  var styleText =
+    "::-moz-focus-inner{border:0 !important;}:focus{outline: none !important;";
+  var unfocus_style = document.createElement("STYLE");
+
+  window.unfocus = function () {
+    document.getElementsByTagName("HEAD")[0].appendChild(unfocus_style);
+
+    document.addEventListener("mousedown", function () {
+      unfocus_style.innerHTML = styleText + "}";
+    });
+    document.addEventListener("keydown", function () {
+      unfocus_style.innerHTML = "";
+    });
+  };
+
+  unfocus.style = function (style) {
+    styleText += style;
+  };
+
+  unfocus();
+})(document, window);
